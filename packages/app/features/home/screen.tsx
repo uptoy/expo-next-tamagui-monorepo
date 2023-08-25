214e633ba68de5d49738984f9e3fd96077a73dc8
uptoy <u24to45y@gmail.com>
2023-08-26 00:28:01 +0900

    test

--- packages/app/features/home/screen.tsx
+++ packages/app/features/home/screen.tsx
@@ -11,39 +11,7 @@ export function HomeScreen() {
 
   return (
     <YStack f={1} jc="center" ai="center" p="$4" space>
-      <YStack space="$4" maw={600} jc="center">
-        <Clock />
-        <H1 ta="center" fontFamily="$silkscreen">
-          Welcome to Tamagui.
-        </H1>
-        <Paragraph ta="center">
-          Here's a basic starter to show navigating from one screen to another. This screen uses the
-          same code on Next.js and React Native.
-        </Paragraph>
-
-        <Separator />
-        <Paragraph ta="center">
-          Made by{' '}
-          <Anchor color="$color12" href="https://twitter.com/natebirdman" target="_blank">
-            @natebirdman
-          </Anchor>
-          ,{' '}
-          <Anchor
-            color="$color12"
-            href="https://github.com/tamagui/tamagui"
-            target="_blank"
-            rel="noreferrer"
-          >
-            give it a ⭐️
-          </Anchor>
-        </Paragraph>
-      </YStack>
-
-      <XStack space="$4" ai="center">
-        <Button {...linkProps}>Link to user</Button>
-      </XStack>
-
-      <SheetDemo />
+      aaaaaaaaaaaaa
     </YStack>
   )
 }
